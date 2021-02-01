6a4a78a240826a634c6e4d6f3d811aad27fe8644
Kathryn E Lloyd <71646243+kel-source@users.noreply.github.com>
2021-02-01 21:47:00 +0000

    Added content paragraph & adjusted spacing

--- src/app.js
+++ src/app.js
@@ -1,2 +1,2 @@
-// let firstName = prompt("What is your first name?");
+// let firstName = prompt("What is your first and last name?");
 // document.body.innerHTML = document.body.innerHTML.replace(/Kat/g, firstName);
