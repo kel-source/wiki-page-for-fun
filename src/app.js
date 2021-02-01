957499c2279a11a8a1a4cf7c41c17d884d8a9519
Kathryn E Lloyd <71646243+kel-source@users.noreply.github.com>
2021-02-01 21:09:28 +0000

    Added content spacing & amended button hover CSS

--- src/app.js
+++ src/app.js
@@ -1,2 +1,2 @@
-let firstName = prompt("What is your first name?");
-document.body.innerHTML = document.body.innerHTML.replace(/Kat/g, firstName);
+// let firstName = prompt("What is your first name?");
+// document.body.innerHTML = document.body.innerHTML.replace(/Kat/g, firstName);
